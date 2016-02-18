ebca364da383671693d64a8e12af2b3b0c14e9d7
Pawan979 <pawan_jkc09@live.com>
2016-02-18 12:47:06 +0400

    Add Composition

---
+++ Composition/Composition/Name.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    class Name
+    {
+        public string sName;
+        private string _sLastName;
+        private string _sFirstName;
+
+        public Name(string sFirstName, string sLastName)
+        {
+            _sFirstName = sFirstName;
+            _sLastName = sLastName;
+            sName = _sFirstName + " " + _sLastName;
+        }
+
+    }
+
+    class Address
+    {
+        public string sAddress;
+        private string _sRoadName;
+        private string _sCity;
+
+        public Address(string sRoadName, string sCity)
+        {
+            _sRoadName = sRoadName;
+            _sCity = sCity;
+            sAddress=  _sRoadName + ", " + _sCity;
+        }
+       
+    }
+
+    class Person 
+    {
+        private Name _objName;
+        private Address _objAddress;
+
+        /*Composition --> Name lifecycle depends on the person.
+                          Normally when the person object is destroyed, the object Name should be as well. */
+        
+       public Person(string sFirstName,string sLastName)
+        {
+            _objName = new Name(sFirstName, sLastName);
+        }
+
+        //Aggregation --> its more like uses-a relationship
+        public void SetAddress(Address objAdress)
+       {
+           _objAddress = objAdress;
+       }
+
+       public void Print()
+       {
+           if (_objAddress == null)
+           {
+               System.Console.WriteLine(string.Format("The person's name is {0}.", _objName.sName));
+
+           }
+               else
+           {
+               System.Console.WriteLine(string.Format("{0} is from {1}.", _objName.sName, _objAddress.sAddress));
+
+           }
+
+
+
+       }
+    }
+   
+}
---
+++ Composition/Composition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Composition
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            // Aggregation --> Both class person and address have their own lifecycle. They are independent.
+            Person p1 = new Person("toto", "titi");
+
+            p1.Print();
+
+            Address a1 = new Address("Royal road", "Rhill");
+            p1.SetAddress(a1);
+
+            p1.Print();
+
+            System.Threading.Thread.Sleep(10000);
+
+        }
+    }
+}
