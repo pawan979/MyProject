458b226034164e09f166aaba6799c0642df1dc25
Pawan979 <pawan_jkc09@live.com>
2016-02-18 16:28:35 +0400

    Add FactoryPattern

---
+++ FactoryPattern/Factory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Factory_Pattern
+{
+    abstract class Page
+    {
+
+    }
+
+    class IntroductionPage : Page
+    {
+    }
+
+    class ContentPage : Page 
+    { 
+    }
+
+    class ConclusionPage : Page
+    {
+    }
+
+
+    abstract class Document
+    {
+        private List<Page> _Pages  = new List<Page>();
+
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+        public List<Page> Pages
+        {
+            get { return _Pages; }
+        }
+
+        public abstract void CreatePages();
+
+    }
+
+    class report : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ConclusionPage());
+        }
+    }
+
+    class biography : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new IntroductionPage());
+            Pages.Add(new ContentPage());
+            Pages.Add(new ConclusionPage());
+        }
+    }
+
+}
---
+++ FactoryPattern/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Factory_Pattern
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Document[] d1 = new Document[2];
+
+            d1[0] = new report();
+            d1[1] = new biography();
+
+            foreach (Document d in d1)
+            {
+                System.Diagnostics.Debug.Write(d.GetType().Name + "\n");
+                foreach (Page p in d.Pages)
+                {
+                    System.Diagnostics.Debug.Write("---> " + p.GetType().Name + "\n");
+                }
+            }
+
+        }
+    }
+}
