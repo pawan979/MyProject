f1d08d1eb1f9873ea6a283a5eda136529eff15f1
Pawan979 <pawan_jkc09@live.com>
2016-02-18 16:29:22 +0400

    Add Observer

---
+++ Observer/App_Code/ConcreteSubject.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer
+{
+    abstract class Subject
+    {
+        private List<Observer> _observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer o in _observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    class ConcreteSubject : Subject
+    {
+        private string _subjectState;
+
+        public string SubjectState
+        {
+            get { return _subjectState; }
+            set { _subjectState = value; }
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class ConcreteObserver : Observer
+    {
+        private string _name;
+        private string _observerState;
+        private ConcreteSubject _subject;
+
+        // Constructor
+        public ConcreteObserver(
+          ConcreteSubject subject, string name)
+        {
+            this._subject = subject;
+            this._name = name;
+        }
+
+        public override void Update()
+        {
+            _observerState = _subject.SubjectState;
+            System.Diagnostics.Debug.Write(string.Format("Observer {0}'s new state is {1} \n",
+              _name, _observerState));
+        }
+
+        // Gets or sets subject
+        public ConcreteSubject Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+}
---
+++ Observer/default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+using System.Web.UI.WebControls;
+using Observer;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void Page_Init(object sender, EventArgs e)
+    {
+      ConcreteSubject s = new ConcreteSubject();
+
+        s.Attach(new ConcreteObserver(s, "X"));
+        s.Attach(new ConcreteObserver(s, "Y"));
+        s.Attach(new ConcreteObserver(s, "Z"));
+
+        s.SubjectState = "ABC";
+        s.Notify();
+
+     }
+
+}
